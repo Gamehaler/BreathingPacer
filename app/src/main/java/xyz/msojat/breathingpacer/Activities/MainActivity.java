1a61f4cb80eb7d27c996d3a105cf24c6423ef285
Marko Šojat <markosojat1@gmail.com>
2016-08-20 17:13:32 +0200

    Added TextView IDs, added TextViews to Java.

--- app/src/main/java/xyz/msojat/breathingpacer/Activities/MainActivity.java
+++ app/src/main/java/xyz/msojat/breathingpacer/Activities/MainActivity.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 import xyz.msojat.breathingpacer.R;
 import xyz.msojat.breathingpacer.custom_view.MyView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean crta = false;
+    private boolean isDrawing = false;
 
-    private Button pocetak;
-    private Button kraj;
-    private Chronometer trajanje;
+    private Button btnStart;
+    private Button btnEnd;
+    private Chronometer chronometer;
+    private TextView tvInhaleTime;
+    private TextView tvExhaleTime;
+    private TextView tvPauseTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +28,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final MyView animacija = (MyView) findViewById(R.id.my_view);
+        init();
 
-        pocetak = (Button) findViewById(R.id.btn_pocetak);
-        kraj = (Button) findViewById(R.id.btn_kraj);
-        trajanje = (Chronometer) findViewById(R.id.chronometer);
+        tvInhaleTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
-        pocetak.setOnClickListener(new View.OnClickListener() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animacija.startAnimating();
-                if(crta == false) {
-                    trajanje.setBase(SystemClock.elapsedRealtime());
-                    trajanje.start();
+                if(isDrawing == false) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    chronometer.start();
                 }
-                crta = true;
+                isDrawing = true;
             }
         });
 
-        kraj.setOnClickListener(new View.OnClickListener() {
+        btnEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animacija.stopAnimating();
-                trajanje.stop();
-                crta = false;
+                chronometer.stop();
+                isDrawing = false;
             }
         });
+    }
+
+    private void init(){
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnEnd = (Button) findViewById(R.id.btn_end);
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        tvInhaleTime = (TextView) findViewById(R.id.tv_inhale_time);
+        tvExhaleTime = (TextView) findViewById(R.id.tv_exhale_time);
+        tvPauseTime = (TextView) findViewById(R.id.tv_pause_time);
     }
 }
